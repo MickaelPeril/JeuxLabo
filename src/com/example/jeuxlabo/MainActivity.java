104896ffb0e951fd6e77c0ee4f6f1ef6d71c3ab0
reshart <reshart@hotmail.fr>
2013-11-08 17:34:27 +0100

    boutton start + methode pour ouvrir

--- src/com/example/jeuxlabo/MainActivity.java
+++ src/com/example/jeuxlabo/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.jeuxlabo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button button1;//boutton start
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		nouvellePage();
+        	}
+        });
+					
+				
 	}
 
 	@Override
@@ -17,6 +32,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void nouvellePage(){ //methode ouverture d'une nouvelle page
+		
+	
 	}
 
 }
