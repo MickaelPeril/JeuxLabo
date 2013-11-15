b9a5e6c0a24960154a2bef4f6f0d3fabbbfb38c0
reshart <reshart@hotmail.fr>
2013-11-15 19:48:29 +0100

    +cercle rouge +depassement du cercle par la biere

--- gen/com/example/jeuxlabo/R.java
+++ gen/com/example/jeuxlabo/R.java
@@ -28,8 +28,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int biere=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int table_campus=0x7f020002;
+        public static final int campusidicon=0x7f020001;
+        public static final int cercle=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int table_campus=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0002;
--- src/com/example/jeuxlabo/BounceView.java
+++ src/com/example/jeuxlabo/BounceView.java
@@ -1,5 +1,7 @@
 package com.example.jeuxlabo;
 
+import com.example.jeuxlabo.R;
+
 import android.content.Context; 
 import android.graphics.Canvas; 
 import android.graphics.Point; 
@@ -11,11 +13,17 @@ import android.widget.ImageView;
 
 public class BounceView extends View { 
 	
+	
+	// boolean si la biere depasse le cercle
+		protected boolean depassement=false;
+		//sprite du cercle rouge
+		protected Drawable mySpriteCircle;
+		
      /* Our Ball together with the location it will be painted*/ 
      protected Drawable mySprite; 
 	
      //protected Point mySpritePos = new Point(0,0); 
-     protected Point mySpritePos = new Point(200,300); 
+     protected Point mySpritePos = new Point(150,200); 
       
      /* Working with a Enum is 10000% 
       * safer than working with int's 
@@ -33,41 +41,56 @@ public class BounceView extends View {
           this.setBackgroundResource( R.drawable.table_campus );
           // Load our "Ball" 
           this.mySprite = this.getResources().getDrawable(R.drawable.biere);
-          
+          this.mySpriteCircle = this.getResources().getDrawable(R.drawable.cercle);
      } 
 
      @Override 
      protected void onDraw(Canvas canvas) { 
           /* Check if the Ball started to leave 
            * the screen on left or right side */ 
-         
-         this.mySpritePos.x -= mPitch; 
-         
-          if (mySpritePos.x >= this.getWidth() - mySprite.getBounds().width()) { 
-              mySpritePos.x = this.getWidth() - mySprite.getBounds().width();
-               //this.myXDirection = HorizontalDirection.LEFT; 
-          } else if (mySpritePos.x <= 0) { 
-              mySpritePos.x = 0;
-               //this.myXDirection = HorizontalDirection.RIGHT; 
-          } 
-          
-         
-         this.mySpritePos.y -= mHeading; 
-          
-          if (mySpritePos.y >= this.getHeight() - mySprite.getBounds().height()) { 
-              mySpritePos.y = this.getHeight() - mySprite.getBounds().height(); 
-         } else if (mySpritePos.y <= 0) { 
-             mySpritePos.y = 0; 
-         } 
- 
+    	 
+    	 if(!depassement){ //si la biere ne sort pas du cercle
+    		 /* Check if the Ball started to leave 
+              * the screen on left or right side */ 
+	         this.mySpritePos.x -= mPitch; 
+	         
+	          if (mySpritePos.x >= this.getWidth() - mySprite.getBounds().width()) { 
+	              mySpritePos.x = this.getWidth() - mySprite.getBounds().width();
+	               //this.myXDirection = HorizontalDirection.LEFT; 
+	          } else if (mySpritePos.x <= 0) { 
+	              mySpritePos.x = 0;
+	               //this.myXDirection = HorizontalDirection.RIGHT; 
+	          } 
+	          
+	         
+	         this.mySpritePos.y -= mHeading; 
+	          
+	          if (mySpritePos.y >= this.getHeight() - mySprite.getBounds().height()) { 
+	              mySpritePos.y = this.getHeight() - mySprite.getBounds().height(); 
+	         } else if (mySpritePos.y <= 0) { 
+	             mySpritePos.y = 0; 
+	         } 
 
+    	 }
 
           /* Set the location, where the sprite 
            * will draw itself to the canvas */ 
+    	 this.mySpriteCircle.setBounds(50, 150, 450,450+100); 
           this.mySprite.setBounds(this.mySpritePos.x, this.mySpritePos.y, 
                     this.mySpritePos.x + 250, this.mySpritePos.y + 250); 
            
           /* Make the sprite draw itself to the canvas */ 
+          this.mySpriteCircle.draw(canvas);
           this.mySprite.draw(canvas); 
+          
+          //collision/depassement
+          if(this.mySpritePos.x < 50 || this.mySpritePos.y < 150 ||   this.mySpritePos.x + 250 > 450 || this.mySpritePos.y + 250 > 550 ){
+        	 fonctionDepassement();
+          }
      } 
+     
+   //fonction de depassement
+     public void  fonctionDepassement(){
+    	 depassement=true;
+     }
 }
